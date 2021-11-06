14e5587e914fbe7466c10739cb02008684e36289
Salman Inamdar <salmaninamdar1@gmail.com>
2021-11-07 00:25:24 +0530

    complexObject_objInObj

---
+++ documentObjectMode/complexObj.js
@@ -0,0 +1,22 @@
+//  Simple obj
+var book = {
+    id: 1001,
+    book_name: "fifty shade",
+    book_color: 'black',
+}
+
+
+// complex obj it means obj inwhich object
+
+var contact ={
+    name:"salman",
+    id: 112,
+    preferences:{
+        pgCount:100,
+        defaultSort:"last page",
+    }
+}
+console.log(contact);
+console.log(typeof (contact));
+console.log(contact.preferences);
+console.log(typeof (contact.preferences));
