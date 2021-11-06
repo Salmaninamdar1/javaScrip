002ba9330986a26c87f33ec083ccec9a4d909b31
Salman Inamdar <salmaninamdar1@gmail.com>
2021-11-07 00:42:07 +0530

    how to create an array in obj

--- documentObjectMode/complexObj.js
+++ documentObjectMode/complexObj.js
@@ -1,22 +1,45 @@
 //  Simple obj
 var book = {
-    id: 1001,
-    book_name: "fifty shade",
-    book_color: 'black',
-}
-
+  id: 1001,
+  book_name: "fifty shade",
+  book_color: "black",
+};
 
 // complex obj it means obj inwhich object
 
-var contact ={
-    name:"salman",
-    id: 112,
-    preferences:{
-        pgCount:100,
-        defaultSort:"last page",
-    }
-}
-console.log(contact);
-console.log(typeof (contact));
-console.log(contact.preferences);
-console.log(typeof (contact.preferences));
+var contact = {
+  name: "salman",
+  id: 112,
+  preferences: {
+    pgCount: 100,
+    defaultSort: "last page",
+  },
+  contactMechanisum: [
+    {
+      phone: 8055565523,
+      name: "salman",
+      type: "empolyee",
+    },
+    {
+      phone: 9890359475,
+      name: "naseem",
+      type: "director",
+    },
+    {
+      phone: 8830493331,
+      name: "Naveed",
+      type: "manager",
+    },
+    {
+      phone: 9730996304,
+      name: "Md Ismail",
+      type: "owner",
+    },
+  ],
+};
+// console.log(contact);
+// console.log(typeof (contact));
+// console.log(contact.preferences);
+// console.log(typeof (contact.preferences));
+
+console.log(contact.contactMechanisum);
