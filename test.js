f9630cb89566f47d4496121e2734b044f490e6df
Salman Inamdar <salmaninamdar1@gmail.com>
2021-12-12 17:40:58 +0530

    problem solving by h rank

---
+++ test.js
@@ -0,0 +1,31 @@
+// var x={},
+//   y={key:"y"},
+//   a={key:"z"},
+//   z={key:"z"};
+// x[y] = 11;
+// x[z] = 21;
+// x[a] = 43;
+// console.log(x[y]);
+
+
+
+// var i=0;
+// do {
+// i++;
+//   if (i==5){
+//      LINE OF CODE GOES HERE
+//      i=0;
+//    }
+// }while(i<7);
+// console.log(i);
+
+
+
+// var cars=2;
+// do{
+//   cars/=2;
+// }while(cars<1);
+
+// console.log("Number of cars: " + cars)
+
+
