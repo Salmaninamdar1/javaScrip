56c4cd2c72f755e5a1db5be5bf974fe564a7de70
Salman Inamdar <salmaninamdar1@gmail.com>
2021-12-27 22:47:51 +0530

    arrays of object methods

---
+++ Obj-method/Obj-array.js
@@ -0,0 +1,12 @@
+//------array method in Object-----
+
+var salman=["hi","salman", "inamdar"]
+var salman1=["hello","Naveed", "khan"];
+let concat= salman.concat(salman1);
+
+// console.log(concat);//[ 'hi', 'salman', 'inamdar', 'hello', 'Naveed', 'khan' ]
+
+var join1=["1","2","3"]
+var join2=["4","5","6"]
+let join =  join1.join(join2);
+console.log(join);
